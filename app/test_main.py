cbdcd8c526a1a8fc40a6ba9854ca547d0f9614b2
Hemanth Degapudi <hemanthreddydegapudi18@gmail.com>
2026-08-20 16:42:33 +0530

    fix: reset test db on each run, ignore test.db

--- app/test_main.py
+++ app/test_main.py
@@ -9,6 +9,7 @@ from app.models import Base
 TEST_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 def override_get_db():
